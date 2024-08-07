95fb7e43b7c3b4bccd127a02f1ed553c81af7f46
Genialqua <fred.aleokemalachi@gmail.com>
2024-08-07 19:36:32 +0000

    Created an arrow function

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const add = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+    };
+
+  this.addNeighborhood = add;
+}
