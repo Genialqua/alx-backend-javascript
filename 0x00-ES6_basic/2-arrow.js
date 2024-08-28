811350501dcdca7bc35fb6c0dcd126f23db220a6
Genialqua <fred.aleokemalachi@gmail.com>
2024-08-28 11:26:33 +0000

    Editted the code for linting

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,13 +1,13 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = [
-'SOMA',
-'Union Square'
-];
+    'SOMA',
+    'Union Square',
+  ];
 
   const add = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
     return this.sanFranciscoNeighborhoods;
-    };
+  };
 
   this.addNeighborhood = add;
 }
