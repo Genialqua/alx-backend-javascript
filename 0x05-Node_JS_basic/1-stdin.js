1b340b92ec528a50c7ae19ed8f961d6aa4318283
Genialqua <fred.aleokemalachi@gmail.com>
2024-12-14 18:20:56 +0000

    Made changes to the file

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,17 +2,17 @@ const { isatty } = require('tty');
 
 process.stdin.setEncoding('utf8');
 
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
 
   const trimmedInput = input.trim();
 
   if (isatty(process.stdin.fd)) {
-    process.stdout.write(`Your name is: ${trimmedInput}\n`);
+    process.stdout.write(`Your name is: ${trimmedInput}`);
     process.exit(); // Exit after displaying the name
   } else {
-    process.stdout.write(`Your name is: ${trimmedInput}\n`);
+    process.stdout.write(`Your name is: ${trimmedInput}`);
     process.stdin.on('end', () => {
       process.stdout.write('This important software is now closing.\n');
     });
