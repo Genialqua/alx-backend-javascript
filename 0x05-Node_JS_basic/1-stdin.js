3b8dce7c073546de4bf598ba47a6553157075c5c
Genialqua <fred.aleokemalachi@gmail.com>
2024-12-14 18:27:06 +0000

    Made changes to the file again

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,7 +5,6 @@ process.stdin.setEncoding('utf8');
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (input) => {
-
   const trimmedInput = input.trim();
 
   if (isatty(process.stdin.fd)) {
